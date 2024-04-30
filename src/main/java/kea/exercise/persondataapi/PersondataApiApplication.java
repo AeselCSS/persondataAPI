8fd66b660204cc3f8c53f7fe8e10f4cdcde4260f
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-04-30 10:03:50 +0200

    initial commit

---
+++ src/main/java/kea/exercise/persondataapi/PersondataApiApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.persondataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersondataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersondataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exercise/persondataapi/PersondataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercise.persondataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersondataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
